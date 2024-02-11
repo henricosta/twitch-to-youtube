001a8b1ee8e54ccf814e7f322064605cc5c93494
henricosta <pedrocosta.lov@gmail.com>
2024-02-11 11:41:41 -0300

    download clip and save file on filepath

---
+++ twitch/download.go
@@ -0,0 +1,46 @@
+package twitch
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func Download(url string, filepath string) error {
+	slug, err := parseClipSlug(url)
+	if err != nil {
+		return err
+	}
+
+	client := &Client{
+		client: &http.Client{},
+		gqlUrl: "https://gql.twitch.tv/gql",
+	}
+
+	clip, err := getClip(client, slug)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Found clip: %v\n", clip.Title)
+
+	videoUrl := getClipAuthenticatedUrl(slug)
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(videoUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
