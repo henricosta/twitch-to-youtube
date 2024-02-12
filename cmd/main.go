fa48215e6fe710c0e3bd60ca098498c5a6941d62
henricosta <pedrocosta.lov@gmail.com>
2024-02-12 16:02:35 -0300

    integrate pieces in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	twyt "github.com/henricosta/twitch-to-youtube/twitchtoyoutube"
+	"google.golang.org/api/youtube/v3"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a url as an argument.")
+		return
+	}
+
+	url := os.Args[1]
+
+	twyt.Download(url, "./clip.mp4")
+
+	upload := &youtube.Video{
+		Snippet: &youtube.VideoSnippet{
+			Title:       "Test Upload",
+			Description: "Test Description",
+		},
+	}
+
+	twyt.UploadVideo("./clip.mp4", upload)
+
+	fmt.Println("Video uploaded successfully!")
 }
