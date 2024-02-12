8980af4fd452001c40c970322d1f8db699d71e7a
henricosta <pedrocosta.lov@gmail.com>
2024-02-12 11:39:32 -0300

    Add UploadVideo function

---
+++ twitchtoyoutube/upload.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+func UploadVideo(filepath string, video *youtube.Video) (videoId string) {
+	service := getService()
+
+	call := service.Videos.Insert([]string{"snippet", "status"}, video)
+
+	file, err := os.Open("test.mp4")
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("Error opening %v: %v", "test.mp4", err)
+	}
+
+	response, err := call.Media(file).Do()
+	if err != nil {
+		log.Fatalf("Error making API call: %v", err.Error())
+	}
+
+	return response.Id
+}
