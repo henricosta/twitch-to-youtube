b51e44fec200ad3b79bd97dc3c0a60fa699f5782
henricosta <pedrocosta.lov@gmail.com>
2024-02-25 10:18:27 -0300

    Refactor download test to include file path check

--- twitchtoyoutube/download_test.go
+++ twitchtoyoutube/download_test.go
@@ -1,12 +1,19 @@
 package twitchtoyoutube
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 var TEST_CLIP_URL = "https://www.twitch.tv/cellbit/clip/AwkwardGlutenFreeMuleStoneLightning-KiUZC6pWzL1DzOq0"
 
 func TestDownload(t *testing.T) {
-	err := Download(TEST_CLIP_URL)
+	filepath, _, err := Download(TEST_CLIP_URL, "./videos")
 	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := os.Stat(filepath); err != nil {
 		t.Fatal(err)
 	}
 }
